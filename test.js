157f348052010a416fb3912e3c4ec6cf14bbd27c
Manuel Medina <108639514+elmamuel45@users.noreply.github.com>
2023-08-17 11:24:56 -0400

    Add files via upload

--- test.js
+++ test.js
@@ -2,10 +2,27 @@
 const assert = require('chai').assert;
 const http = require('http');
 
-// Ajusta la ruta para subir un nivel y acceder a app.js
-const server = require('./app');
+const server = require('../app'); // Ajusta la ruta si es necesario
 
 describe('Aplicación web', () => {
+  let appServer;
+
+  // Antes de las pruebas, inicia el servidor
+  before((done) => {
+    appServer = server.listen(3000, () => {
+      console.log('Servidor de prueba iniciado en el puerto 3000');
+      done();
+    });
+  });
+
+  // Después de las pruebas, detén el servidor
+  after((done) => {
+    appServer.close(() => {
+      console.log('Servidor de prueba detenido');
+      done(); // Asegurarse de que Mocha espere hasta que se detenga el servidor
+    });
+  });
+
   it('Debe retornar un mensaje exitoso', (done) => {
     http.get('http://localhost:3000', (res) => {
       let data = '';
